a84950d509c2ff6a65e59bdb743a087f2902b692
dedenistiawan <dedenistiawan@gmail.com>
2023-06-27 22:19:43 +0700

    initial commit

--- R/Fgwc ver 1.R
+++ R/Fgwc ver 1.R
@@ -1,4 +1,5 @@
 # Load Data Sovi
+load("D:/My Drive/deden-RMarkdown & Github/PDP-SoVI/Data/sovi_data.RData")
 load("D:/My Drive/deden-RMarkdown & Github/PDP-SoVI/Data/sovi_data_Kab_R.RData")
 # Load Data Matrix Distance
 load("D:/My Drive/deden-RMarkdown & Github/PDP-SoVI/Data/sovi_dist.rdata")
@@ -14,8 +15,8 @@ library(naspaclust)
 library(rdist)
 
 # Pentuan Parameter fgwc
-param_fgwc <- c(kind='v',ncluster=4,m=2,distance='euclidean',order=3,
-                alpha=0.5,a=1.2,b=1.2,max.iter=1000,error=1e-6,randomN=0)
+param_fgwc <- c(kind='v',ncluster=3,m=2,distance='euclidean',order=2,
+                alpha=0.7,a=1,b=1,max.iter=1000,error=1e-5,randomN=0)
 
 # FGWC Standart
 Res_fgwc <- fgwc(data=sovi_data_Kab_R, pop=Sovi_Pop, distmat=mat_dist, algorithm = "classic", param_fgwc,1)
