47b8cbc1ee4c04ab14c9ae1bd813a52ab509bcbd
dedenistiawan <dedenistiawan@gmail.com>
2023-07-04 14:18:17 +0700

    initial commit

--- R/Fgwc ver 1.R
+++ R/Fgwc ver 1.R
@@ -9,14 +9,13 @@ load("D:/My Drive/deden-RMarkdown & Github/PDP-SoVI/Data/sovi_pop.rdata")
 #Mengubah data frame Sovi distance menjadi matrik distance
 mat_dist= data.matrix(sovi_distance)
 
-
 # Library yag dibutuhkan
 library(naspaclust)
 library(rdist)
 
 # Pentuan Parameter fgwc
-param_fgwc <- c(kind='v',ncluster=3,m=2,distance='euclidean',order=2,
-                alpha=0.7,a=1,b=1,max.iter=1000,error=1e-5,randomN=0)
+param_fgwc <- c(kind='u',ncluster=4,m=1.5,distance='minkowski',order=2,
+                alpha=0.7,a=1,b=1,max.iter=500,error=1e-5,randomN=10)
 
 # FGWC Standart
 Res_fgwc <- fgwc(data=sovi_data_Kab_R, pop=Sovi_Pop, distmat=mat_dist, algorithm = "classic", param_fgwc,1)
