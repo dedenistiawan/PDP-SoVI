7bb5970340585fb539b1719fc4d6bc5e76c18d95
dedenistiawan <dedenistiawan@gmail.com>
2023-07-12 21:35:11 +0700

    initial commit

--- R/Fgwc ver 1.R
+++ R/Fgwc ver 1.R
@@ -1,13 +1,22 @@
 # Load Data Sovi
 load("D:/My Drive/deden-RMarkdown & Github/PDP-SoVI/Data/sovi_data.RData")
 load("D:/My Drive/deden-RMarkdown & Github/PDP-SoVI/Data/sovi_data_Kab_R.RData")
+load("D:/My Drive/deden-RMarkdown & Github/PDP-SoVI/Data/sovi_data_Kab_514.RData")
 # Load Data Matrix Distance
-load("D:/My Drive/deden-RMarkdown & Github/PDP-SoVI/Data/sovi_dist.rdata")
+# Load Data Matrix Distance
+load("D:/My Drive/deden-RMarkdown & Github/PDP-SoVI/Data/sovi_dist_514.Rdata")
 #Load Data Populasi
+#merubah df to vector
+#sovi_data_pop_514 <- read_excel("Data/sovi_data_pop_514.xlsx")
+#v1<-sovi_data_pop_514$A 
 load("D:/My Drive/deden-RMarkdown & Github/PDP-SoVI/Data/sovi_pop.rdata")
+load("D:/My Drive/deden-RMarkdown & Github/PDP-SoVI/Data/pop.rdata")
+load("D:/My Drive/deden-RMarkdown & Github/PDP-SoVI/Data/Sovi_pop_514.rdata")
+
 
 #Mengubah data frame Sovi distance menjadi matrik distance
 mat_dist= data.matrix(sovi_distance)
+mat_dist= data.matrix(Distance_matrix_514)
 
 # Library yag dibutuhkan
 library(naspaclust)
@@ -15,12 +24,14 @@ library(rdist)
 
 # Pentuan Parameter fgwc
 param_fgwc <- c(kind='u',ncluster=4,m=1.5,distance='minkowski',order=2,
-                alpha=0.8,a=1,b=1,max.iter=1000,error=1e-5,randomN=10)
+                alpha=0.7,a=1,b=1,max.iter=1000,error=1e-5,randomN=10)
 
 # FGWC Standart
-Res_fgwc <- fgwc(data=sovi_data_Kab_R, pop=Sovi_Pop, distmat=mat_dist, algorithm = "classic", param_fgwc,1)
+Res_fgwc <- fgwc(data=sovi_data_kab_full_R, pop=Sovi_Pop, distmat=mat_dist, algorithm = "classic", param_fgwc,1)
+Res_fgwc <- fgwc(data=sovi_data_kab_full_R, pop=v1, distmat=mat_dist, algorithm = "classic", param_fgwc,1)
 
 # Menggabungkan hasil cluster algoritma FGWC dengan data
 library(xlsx)
 cluster.output <- cbind(data_kab ,Res_fgwc$cluster)
-write.xlsx(cluster.output, file = "Final Cluster3m1-5.xlsx", row.names = TRUE)
+cluster.output <- cbind(data_kab_geoda_514 ,Res_fgwc$cluster)
+write.xlsx(cluster.output, file = "Final Cluster514.xlsx", row.names = TRUE)
