1abf6ed5b4b381c8c7b507352e493bbcf6ecf20b
dedenistiawan <dedenistiawan@gmail.com>
2023-07-13 10:51:21 +0700

    initial commit

--- R/Fgwc ver 2.R
+++ R/Fgwc ver 2.R
@@ -1,5 +1,5 @@
 # Load Data Sovi
-load("D:/My Drive/deden-RMarkdown & Github/PDP-SoVI/Data/sovi_data.RData")
+load("D:/My Drive/deden-RMarkdown & Github/PDP-SoVI/Data/sovi_data_Kab_R.RData")
 
 # Load Data Matrix Distance
 load("D:/My Drive/deden-RMarkdown & Github/PDP-SoVI/Data/sovi_dist.Rdata")
@@ -19,7 +19,8 @@ param_fgwc <- c(kind='u',ncluster=4,m=1.5,distance='minkowski',order=2,
                 alpha=0.7,a=1,b=1,max.iter=1000,error=1e-5,randomN=10)
 
 # FGWC Standart
-Res_fgwc <- fgwc(data=sovi_data, pop=Sovi_Pop, distmat=mat_dist, algorithm = "classic", param_fgwc,1)
+Res_fgwc <- fgwc(data=sovi_data_Kab_R, pop=Sovi_Pop, distmat=mat_dist, algorithm = "classic", param_fgwc,1)
+
 
 # Menggabungkan hasil cluster algoritma FGWC dengan data
 library(xlsx)
